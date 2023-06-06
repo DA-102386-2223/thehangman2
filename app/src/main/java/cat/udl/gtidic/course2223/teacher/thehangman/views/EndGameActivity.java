129b8a7effa659ce28c04365e6029a37416aabde
Carlos <carlosmg.12@hotmail.es>
2023-06-06 17:02:08 +0200

    Ampliació 2: Compte enrrera + archius que no se

--- app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/views/EndGameActivity.java
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/views/EndGameActivity.java
@@ -5,19 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import cat.udl.gtidic.course2223.teacher.thehangman.R;
 
+
+import android.os.CountDownTimer;
+
+import android.widget.TextView;
+
+
 public class EndGameActivity extends AppCompatActivity {
 
+    private TextView countdownText;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
 
-        Button btnRestart = findViewById(R.id.return_init);
+        countdownText = findViewById(R.id.countdown_text); // You need to have this TextView in your layout
         ImageView endGameImage = findViewById(R.id.end_game_image);
 
         Bundle extras = getIntent().getExtras();
@@ -29,10 +37,22 @@ public class EndGameActivity extends AppCompatActivity {
             endGameImage.setImageResource(R.drawable.loser);
         }
 
-        btnRestart.setOnClickListener(view -> {
-            Intent intent = new Intent(this, InitActivity.class);
-            startActivity(intent);
-            finish();
-        });
+        startCountdown();
+    }
+
+    private void startCountdown() {
+        new CountDownTimer(5000, 1000) { // 5000 ms (or 5 seconds) with interval of 1000 ms (or 1 second)
+
+            public void onTick(long millisUntilFinished) {
+                countdownText.setText(String.valueOf(millisUntilFinished / 1000)); // update the countdown text
+            }
+
+            public void onFinish() {
+                Intent intent = new Intent(EndGameActivity.this, InitActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
     }
 }
+
