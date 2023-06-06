f15d20de2800a8bafbcb5b08a5dbe7a7ad50bdcf
Victor Cunillera <victor2003cn@gmail.com>
2023-06-06 15:48:51 +0200

    Requisit 1.2

--- app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/views/HowToPlayActivity.java
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/views/HowToPlayActivity.java
@@ -3,15 +3,21 @@ package cat.udl.gtidic.course2223.teacher.thehangman.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import cat.udl.gtidic.course2223.teacher.thehangman.R;
 
 public class HowToPlayActivity extends AppCompatActivity {
+
+    TextView textView = findViewById(R.id.tvDescription);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_play);
 //        nothing interesting here
+
+        textView.setMovementMethod(new ScrollingMovementMethod());
     }
 }
