7e2b6e2ecd577a722104e83e69265422e415c69d
David <daviidsolv@gmail.com>
2023-06-06 15:36:53 +0200

    requisit4

--- app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/EndGameActivity.java
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/EndGameActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,13 +21,16 @@ public class EndGameActivity extends AppCompatActivity {
         isPlayerWinner = i.getBooleanExtra("isPlayerWinner", false);
 
         TextView tv = findViewById(R.id.endGameTextView);
-        Button btn = findViewById(R.id.endGameButton);
 
         if (isPlayerWinner) tv.setText("Has guanyat!");
         else tv.setText("Has perdut!");
 
-        btn.setOnClickListener(v -> {
-            finish();
-        });
+        new CountDownTimer(5000, 1000) {
+            public void onFinish() {
+                finish();
+            }
+            public void onTick(long millisUntilFinished) {
+            }
+        }.start();
     }
 }
