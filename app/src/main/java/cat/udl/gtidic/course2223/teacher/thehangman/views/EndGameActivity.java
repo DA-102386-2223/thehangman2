ab86f242b7fbbc180b72e8ebddcf66e2cf080e37
paula <paulasilland@gmail.com>
2023-06-06 16:07:45 +0200

    requisit3

---
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/views/EndGameActivity.java
@@ -0,0 +1,16 @@
+package cat.udl.gtidic.course2223.teacher.thehangman.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cat.udl.gtidic.course2223.teacher.thehangman.R;
+
+public class EndGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_game);
+    }
+}
