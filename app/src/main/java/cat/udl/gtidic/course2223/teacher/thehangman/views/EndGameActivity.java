43a784267f8ac1997b88898c9eac7cbdf2eee5c7
LordVader03 <pelliterogarciaalex@gmail.com>
2023-06-06 16:48:33 +1100

    Ampliació 2: End Game v2 Done

--- app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/views/EndGameActivity.java
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/views/EndGameActivity.java
@@ -3,18 +3,25 @@ package cat.udl.gtidic.course2223.teacher.thehangman.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import cat.udl.gtidic.course2223.teacher.thehangman.R;
 
 public class EndGameActivity extends AppCompatActivity {
     TextView ResultText;
+    TextView noTimeToDieText;
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
         ResultText = findViewById(R.id.ResultText);
+        noTimeToDieText = findViewById(R.id.noTimeToDieText);
+        findViewById(R.id.ReturnButton).setOnClickListener(v -> finish());
+        findViewById(R.id.ReturnButton).setVisibility(View.INVISIBLE);
         Bundle bundle = getIntent().getExtras();
         if(bundle != null) {
             boolean resultado = bundle.getBoolean("resultado");
@@ -24,6 +31,26 @@ public class EndGameActivity extends AppCompatActivity {
                 ResultText.setText("Perdiste, admítelo no sabes jugar :(");
             }
         }
-        findViewById(R.id.ReturnButton).setOnClickListener(v -> finish());
+        handler = new Handler();
+        noTimeToDie();
+    }
+    public void noTimeToDie(){
+        int i;
+        for (i = 5; i > 0; i--) {
+            final int countdownValue = i;
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    noTimeToDieText.setText(String.valueOf(countdownValue));
+                }
+            }, (5 - i) * 1000);
+        }
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                noTimeToDieText.setText("0");
+                finish();
+            }
+        }, 5000);
     }
 }
